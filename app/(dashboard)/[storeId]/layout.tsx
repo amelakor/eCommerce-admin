8a032e250088c595fd55f9f3adec5600e441092b
amelakor <amela_korjenic@hotmail.com>
2023-08-16 16:35:21 +0200

    Navbar

--- app/(dashboard)/[storeId]/layout.tsx
+++ app/(dashboard)/[storeId]/layout.tsx
@@ -2,6 +2,7 @@ import prismadb from "@/lib/prismadb";
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
+import Navbar from "@/components/Navbar";
 export default async function DashboardLayout({
     children,
     params,
@@ -28,7 +29,7 @@ export default async function DashboardLayout({
 
     return (
         <>
-            <div>Navbar</div>
+            <Navbar />
 
             {children}
         </>
