1228b4feede31ab25dcbb33b57d6814796554b85
amelakor <amela_korjenic@hotmail.com>
2023-08-21 16:09:04 +0200

    Search fix

--- app/(dashboard)/[storeId]/(routes)/categories/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/categories/components/client.tsx
@@ -34,7 +34,7 @@ const CategoriesClient: React.FC<CategoryClientProps> = ({ data }) => {
                 </Button>
             </div>
             <Separator />
-            <DataTable searchKey="label" columns={columns} data={data} />
+            <DataTable searchKey="name" columns={columns} data={data} />
             <Heading title="API" description="API Calls for Categories" />
             <Separator />
             <ApiList entityName="categories" entityIdName="categoryId" />
