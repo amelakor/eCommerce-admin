6dd5c971ef08f02df094541991c5a2d50123ab5d
amelakor <amela_korjenic@hotmail.com>
2023-08-24 16:22:22 +0200

    Stripe

---
+++ app/api/[storeId]/checkout/route.ts
@@ -0,0 +1,92 @@
+import Stripe from "stripe";
+import { NextResponse } from "next/server";
+
+import { stripe } from "@/lib/stripe";
+import prismadb from "@/lib/prismadb";
+
+const corsHeaders = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, POST, PATCH, DELETE, PUT, OPTIONS",
+    "Access-Control-Allow-Headers": "Content-Type, Authorization",
+};
+
+export async function OPTIONS() {
+    return NextResponse.json({}, { headers: corsHeaders });
+}
+
+export async function POST(
+    request: Request,
+    { params }: { params: { storeId: string } }
+) {
+    const { productIds } = await request.json();
+
+    if (!productIds) {
+        return new NextResponse("No product ids provided.", {
+            status: 400,
+            headers: corsHeaders,
+        });
+    }
+
+    const products = await prismadb.product.findMany({
+        where: {
+            id: {
+                in: productIds,
+            },
+        },
+    });
+
+    const line_items: Stripe.Checkout.SessionCreateParams.LineItem[] = [];
+
+    products.forEach((product) => {
+        line_items.push({
+            price_data: {
+                currency: "usd",
+                product_data: {
+                    name: product.name,
+                },
+                unit_amount: product.price * 100,
+            },
+            quantity: 1,
+        });
+    });
+
+    const order = await prismadb.order.create({
+        data: {
+            storeId: params.storeId,
+            isPaid: false,
+            address: "",
+            orderItems: {
+                create: products.map((product) => ({
+                    product: {
+                        connect: {
+                            id: product.id,
+                        },
+                    },
+                })),
+            },
+        },
+    });
+
+    const session = await stripe.checkout.sessions.create({
+        line_items,
+        mode: "payment",
+        billing_address_collection: "required",
+        phone_number_collection: {
+            enabled: true,
+        },
+        success_url: `${process.env.NEXT_PUBLIC_URL}/cart?success=1`,
+        cancel_url: `${process.env.NEXT_PUBLIC_URL}/cart?canceled=1`,
+        metadata: {
+            orderId: order.id,
+        },
+    });
+
+    return NextResponse.json(
+        {
+            url: session.url,
+        },
+        {
+            headers: corsHeaders,
+        }
+    );
+}
